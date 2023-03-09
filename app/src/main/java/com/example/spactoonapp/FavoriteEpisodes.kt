62737b6d5e4b3e633bccb0cc9889ebae23ea85c0
mhmoudhabib <mhmoudrhabib@gmail.com>
2023-03-09 22:20:02 +0200

    design implementation of favorite episodes screen

---
+++ app/src/main/java/com/example/spactoonapp/FavoriteEpisodes.kt
@@ -0,0 +1,11 @@
+package com.example.spactoonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteEpisodes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_episodes)
+    }
+}
